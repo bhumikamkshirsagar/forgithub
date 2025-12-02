78e690e65d67b5721e6325fe29a79a78f7d04933
bhumikamkshirsagar <bhumikamkshirsagar@gmail.com>
2025-12-02 08:27:29 +0530

    firstcommit

---
+++ forgithub/src/main/java/forgithub/forgithub/ForgithubApplication.java
@@ -0,0 +1,13 @@
+package forgithub.forgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgithubApplication.class, args);
+	}
+
+}
---
+++ forgithub/src/test/java/forgithub/forgithub/ForgithubApplicationTests.java
@@ -0,0 +1,13 @@
+package forgithub.forgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
